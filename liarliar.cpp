661cdf63acf175e2dbbe5c226cb960d315eb4028
Jeff Chapman II <jac@JAChapmanII.net>
2011-07-19 18:18:01 -0400

    Add liarliar solution

---
+++ liarliar.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#define X for(j=0;j<Y.s;++j){F(i)if(!strcmp(Y.c[j],Z.n))break;}if
+#define Z z[i]
+#define Y z[c]
+#define C char*
+#define F(x) for(x=0;x<p;++x){
+struct n{int g,s;C*c,*n;};main(int a,C*v){std::ifstream f;f.open(v[1]);int p,i,j,c,d,b=a=0;f>>p;n*z=new n[p];F(i)Z.g=0;Z.n=new char[9];f>>Z.n;f>>c;Z.c=new C[c];Z.s=c;for(j=0;j<c;++j){Z.c[j]=new char[9];f>>Z.c[j];}}z[0].g=1;F(d)F(c)if(!Y.g)X(Z.g)Y.g=-Z.g;}if(Y.g)X(!Z.g)Z.g=-Y.g;}}}F(c)(Y.g+1?a:b)++;}i=a,j=b;if(a<b)i=b,j=a;std::cout<<i<<" "<<j<<'\n';}
