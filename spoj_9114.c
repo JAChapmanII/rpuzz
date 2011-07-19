8df792e623b7d9ef2aa72559da3b1a342ede60a5
Jeff Chapman II <jac@JAChapmanII.net>
2011-07-19 18:24:15 -0400

    Remove two extra characters

--- spoj_9114.c
+++ spoj_9114.c
@@ -1,2 +1,2 @@
 #include <stdio.h>
-main(c,v){long p,z,s,a,b,m,n,f;scanf("%ld",&z);for(p=0;p<z;p++){a=b=c=0;while((c=getchar())!=' ')(c==97)?a++:(c==98)?b++:0;scanf("%ld",&s);m=n=1;c=1;while(c<s+1)f=m+n,n=m,m=f,c++;printf("%ld\n",a*n+b*m);}}
+main(c,v){long p,z,s,a,b,m,n,f;scanf("%ld",&z);for(p=0;p<z;p++){a=b=c=0;while((c=getchar())!=' ')(c==97)?a++:(c==98)?b++:0;scanf("%ld",&s);m=n=c=1;while(c<s+1)f=m+n,n=m,m=f,c++;printf("%ld\n",a*n+b*m);}}
