9c931b0e6ad518ed9a96eb22133e7c3976a9072c
Jeff Chapman II <jac@JAChapmanII.net>
2011-07-22 06:22:06 -0400

    Move an increment, remove two characters

--- spoj_9114.c
+++ spoj_9114.c
@@ -1,2 +1,2 @@
 #include <stdio.h>
-main(c,v){long p,z,s,a,b,m,n;scanf("%ld",&z);for(p=0;p<z;p++){a=b=c=0;while((c=getchar())!=' ')(c==97)?a++:(c==98)?b++:0;scanf("%ld",&s);m=n=c=1;while(c<s+1)m+=n,n=m-n,c++;printf("%ld\n",a*n+b*m);}}
+main(c,v){long p,z,s,a,b,m,n;scanf("%ld",&z);for(p=0;p<z;p++){a=b=c=0;while((c=getchar())!=' ')(c==97)?a++:(c==98)?b++:0;scanf("%ld",&s);m=n=c=1;while(c++<s+1)m+=n,n=m-n;printf("%ld\n",a*n+b*m);}}
